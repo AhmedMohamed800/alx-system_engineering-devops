93490786efe12734aa632053ef1b06771d3d77e1
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-11-07 09:49:31 +0000

    Add 2-recurse.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """return the number of subscribers"""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     header = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=header)
+    response = requests.get(url, headers=header, allow_redirects=False)
     if response.status_code == 200:
         json_data = response.json()
         children = json_data.get('data').get('children')
