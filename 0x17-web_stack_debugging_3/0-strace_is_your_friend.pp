d1adef994c548031afbdc0daf0e7775995b53e88
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-11-08 18:34:47 +0000

    Add  0-strace_is_

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # replace phpp with php
 
 exec { 'fixPhp':
-   provider    => shell,
-   command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+provider => shell,
+command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 }
