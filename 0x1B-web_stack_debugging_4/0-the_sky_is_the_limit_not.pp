a541365249a56a10915ae36cfa65bad76802d712
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-11-16 10:48:02 +0000

    run!

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # fix cuncurrent users number
 exec {'cuncurrent':
   command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart'
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
+  path    => ['/usr/bin', '/usr/sbin', '/bin', '/etc/init.d/', '/usr/local/bin/:/bin']
 }
