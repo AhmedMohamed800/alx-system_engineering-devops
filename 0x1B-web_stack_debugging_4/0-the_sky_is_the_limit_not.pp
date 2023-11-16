34397fa0419c955074eaaf1170e34998bc509731
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-11-16 11:25:08 +0000

    A0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,14 @@
-# fix cuncurrent users number
-exec {'cuncurrent':
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx && sudo service nginx restart'
+# Increse the request's limit
+
+# Increse ulimit value
+exec { 'fix-config-nginx':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
+
+# Restart nginex service
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
