2e2454f5559f979f971433c6673d7bded45b3a1a
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-11-16 10:28:42 +0000

    edit e_lim

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # fix cuncurrent users number
 exec {'fix':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart"
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf && sudo service nginx restart"
   path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
